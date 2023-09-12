2d66cbc242c72e2d1476f24593010484a478d344
austinbrot <abrotman@stanford.edu>
2023-09-11 23:36:04 -0700

    update file paths for sherlock

--- Analysis/batch_MSC_analyses.m
+++ Analysis/batch_MSC_analyses.m
@@ -1,41 +1,54 @@
-MSCnums = 1:10;
+MSCnums = 1:1;
 edgethresh = .5;
 xdist = 30;
 thresholds = [.003 .004 .005:.005:.05];
 
 
 %% Analyses to run
-run_vertexwise_infomap = 1;
+run_vertexwise_infomap = 0;
 run_parcellation = 1;
 make_parcel_corrmats = 1;
 make_parcel_distmats = 1;
 run_parcel_infomap = 1;
 run_spring_embedding = 1;
 
+home_dir = getenv('HOME');
+oak_dir = getenv('OAK');
+out_dir = [home_dir '/MSCcodebase/results'];
+MSC_dir = [oak_dir '/inprocess/MSC/ds000224'];
+derivatives_dir = [oak_dir '/inprocess/MSC/ds000224-fmriprep'];
+surface_pipeine_dir = [derivatives_dir '/surface_pipeline'];
 
+sessions = {'01'};
+% sessions = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
 
 
 for MSCnum = MSCnums
     
     MSCname = ['MSC' sprintf('%02i',MSCnum)];
     %%
-    infomap_outfolder = ['/data/nil-bluearc/GMT/Evan/MSC/Analysis_V1/infomap/' MSCname '_infomap_p003_p005_p05/'];
-    parcellation_outfolder = ['/data/nil-bluearc/GMT/Evan/MSC/Analysis_V1/parcels/'];
-    springembed_outfolder = ['/data/nil-bluearc/GMT/Evan/MSC/Analysis_V1/spring_embed/'];
-    parcelinfomap_outfolder = ['/data/nil-bluearc/GMT/Evan/MSC/Analysis_V1/parcels/' MSCname '_parcels_LR_infomap_p003_p05/'];
-    surfdir = ['/data/nil-bluearc/GMT/Laumann/MSC/fs5.3_native/FREESURFER_fs_LR/' MSCname '/7112b_fs_LR/fsaverage_LR32k/'];
-    dmatname = [infomap_outfolder '/distmat_surf_geodesic_vol_euc_xhem_large_uint8.mat'];
+    infomap_outfolder = [out_dir '/infomap/' MSCname '_infomap_p003_p005_p05/'];
+    parcellation_outfolder = [out_dir '/parcels/'];
+    springembed_outfolder = [out_dir '/spring_embed/'];
+    parcelinfomap_outfolder = [parcellation_outfolder '/' MSCname '_parcels_LR_infomap_p003_p05/'];
+    surfdir = [surface_pipeine_dir '/' MSCname '/fs_LR_Talairach/fsaverage_LR32k/'];
+    dmatname = [surface_pipeine_dir '/' MSCname '/cifti_distances/distmat_surf_geodesic_vol_euc_xhem_large_uint8.mat'];
         
     
     
     tmaskfile = ['/data/nil-bluearc/GMT/Laumann/MSC/' MSCname '/' MSCname '_TMASKLIST.txt'];
-    [subjectlist, tmask_list] = textread(tmaskfile,'%s %s');
-    ciftifiles = cell(length(subjects),1);
-    for s = 1:length(subjectlist)
-        ciftifiles{s} = ['/data/nil-bluearc/GMT/Laumann/MSC/' MSCname '/Functionals/FCPROCESS_SCRUBBED_UWRPMEAN/cifti_timeseries_smallwall_native_freesurf/' subjectlist{s} '_LR_surf_subcort_333_32k_fsLR_smooth2.55.dtseries.nii'];
+    % [subjectlist, tmask_list] = textread(tmaskfile,'%s %s');
+    ciftifiles = cell(length(sessions),1);
+    tmask_list = cell(length(sessions),1);
+    rest_dir = [surface_pipeine_dir '/sub-' MSCname '/processed_restingstate_timecourses'];
+    for s = 1:length(sessions)
+        ses_dir = [rest_dir '/ses-func' sessions{s} '/cifti'];
+        ciftifiles{s} = [ses_dir '/sub-' MSCname '_ses-func' sessions{s} '_task-rest_bold_32k_fsLR.dtseries.nii'];
+        tmask_list{s} = [ses_dir '/sub-' MSCname '_ses-func' sessions{s} '_task-rest_bold_32k_fsLR_tmask.txt'];
     end
         
     
+    % TODO: Figure out what this line should be
     ciftidata = ['/data/nil-bluearc/GMT/Evan/MSC/subjects/' MSCname '/cifti/cifti_timeseries_normalwall/RSFC_LR_surf_subcort_333_32k_fsLR_smooth2.55.dtseries.nii'];
     parcellation_file = [parcellation_outfolder '/' MSCname '_parcels_LR.dtseries.nii'];
     
@@ -45,22 +58,23 @@ for MSCnum = MSCnums
     %% vertexwise infomap
     if run_vertexwise_infomap
         
-        distances = smartload(['/data/nil-bluearc/GMT/Laumann/MSC/' MSCname '/normalwall_distmat_333_native_freesurf/distmat_surf_geodesic_vol_euc.mat']);
-        distances = uint8(distances);
-        distances(1:29696,29697:59412) = 255;
-        distances(29697:59412,1:29696) = 255;
-        save(dmatname,'distances','-v7.3');
-        clear distances
+        % distances = smartload(['/data/nil-bluearc/GMT/Laumann/MSC/' MSCname '/normalwall_distmat_333_native_freesurf/distmat_surf_geodesic_vol_euc.mat']);
+        % distances = uint8(distances);
+        % distances(1:29696,29697:59412) = 255;
+        % distances(29697:59412,1:29696) = 255;
+        % save(dmatname,'distances','-v7.3');
+        % clear distances
         
         cd(infomap_outfolder);
         
-        for s = 1:length(subjectlist)
+        for s = 1:length(sessions)
             tmask = load(tmask_list{s}); 
             data = ft_read_cifti_mod(ciftifiles{s});
+            session_data = data.data(:,logical(tmask))
             if s==1
-                alldata = data.data(:,logical(tmask));
+                alldata = session_data;
             else
-                alldata = [alldata data.data(:,logical(tmask))];
+                alldata = [alldata session_data];
             end
             data.data = [];
         end
@@ -74,7 +88,7 @@ for MSCnum = MSCnums
         structure_indices = structure_indices(structure_indices>0);
         structure_indices = (structure_indices > 2) +1;
         
-        dmatname = [infomap_outfolder '/distmat_surf_geodesic_vol_euc_xhem_large_uint8.mat'];
+        % dmatname = [infomap_outfolder '/distmat_surf_geodesic_vol_euc_xhem_large_uint8.mat'];
         
         Run_Infomap_2015(corrmat, dmatname, xdist, thresholds, 0, infomap_outfolder, 8, structure_indices);
         clear corrmat
@@ -97,7 +111,7 @@ for MSCnum = MSCnums
         mkdir([parcellation_outfolder '/' MSCname '/'])
         cd([parcellation_outfolder '/' MSCname '/'])
         
-        for s = 1:length(subjectlist)
+        for s = 1:length(sessions)
             tmask = load(tmask_list{s}); 
             data = ft_read_cifti_mod(ciftifiles{s});
             if s==1
@@ -108,30 +122,36 @@ for MSCnum = MSCnums
             data.data = [];
         end
         
+        disp('Creating corrmat...');
         corrmat = paircorr_mod(alldata');
         clear alldata
         
+        disp('Transforming corrmat...');
         corrmat(isnan(corrmat)) = 0;
         corrmat = FisherTransform(corrmat);
         
         data.data = corrmat;
         clear corrmat;
         
+        disp('Parcellating surface...');
         surface_parcellation_singlesub(MSCname,data,surfdir,1,0,[parcellation_outfolder '/' MSCname '/'])
         
         parcel_creator_cifti('corrofcorr_allgrad_LR_subcort_smooth2.55_wateredge_avg.dtseries.nii',[MSCname '_parcels'],edgethresh,ciftidata)
         movefile([parcellation_outfolder '/' MSCname '/' MSCname '_parcels_edgethresh_' num2str(edgethresh) '.dtseries.nii'],parcellation_file);
         try delete([parcellation_outfolder '/corrofcorr_allgrad_LR_subcort_smooth2.55.dtseries.nii']); catch; end
         
+        disp('Finished parcellation!')
     end
     
     %% parcel corrmats
     if make_parcel_corrmats
+
+        disp('Making parcel corrmats...')
         
         parcels = ft_read_cifti_mod(parcellation_file);
         parcelIDs = unique(parcels.data); parcelIDs(parcelIDs<1) = [];
         
-        for s = 1:length(subjectlist)
+        for s = 1:length(sessions)
             tmask = load(tmask_list{s}); 
             data = ft_read_cifti_mod(ciftifiles{s});
             if s==1
@@ -158,6 +178,8 @@ for MSCnum = MSCnums
     
     %% parcel distmats
     if make_parcel_distmats
+
+        disp('Making parcel distmats...')
         
         distances = smartload(dmatname);
         
@@ -179,6 +201,8 @@ for MSCnum = MSCnums
     
     %% parcel infomap
     if run_parcel_infomap
+
+        disp('running parcel infomap')
         
         mkdir(parcelinfomap_outfolder)
         cd(parcelinfomap_outfolder)
